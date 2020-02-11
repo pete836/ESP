ef8b6f8017a43c6d77524fcb5223a7fa9021edbc
pete836 <60779484+pete836@users.noreply.github.com>
2020-02-11 22:08:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+# import time
+import machine
+import micropython
+import dht
+import umqtt.robust
+
+
+MQTT_CLIENT_ID = "ESP32_Hacienda"
+MQTT_SERVER = "10.0.200.102"
+MQTT_PORT = 1883
+MQTT_TOPIC_TEMP = "pete/test/temperature"
+MQTT_TOPIC_HUM = "pete/test/humidity"
+MQTT_TOPIC_CTRL = "pete/test/control"
+MQTT_TOPIC_BAZINGA = "pete/test/BAZINGA"
+
+
+class Tick:
+    def __init__(self, sensor, mqtt_client, topic_temp, topic_hum):
+        self.sensor = sensor
+        self.mqtt_client = mqtt_client
+        self.topic_temp = topic_temp
+        self.topic_hum = topic_hum
+
+    def aquire(self):
+        self.sensor.measure()
+        temp = self.sensor.temperature()
+        hum = self.sensor.humidity()
+        return temp, hum
+
+    def do(self, unused_t):
+        temp, hum = self.aquire()
+
+        print("Temperature: %3.1f C" % temp)
+        print("Humidity: %3.1f %%" % hum)
+
+        self.mqtt_client.publish(self.topic_temp, str(temp))
+        self.mqtt_client.publish(self.topic_hum, str(hum))
+
+
+class Dioder:
+    def __init__(self, led):
+        self.led = led
+
+    def message_callback(self, topic, msg):
+        print("topic:", topic, "msg:", msg)
+        if topic == b"pete/test/control":
+            self.led_callback(msg)
+        else:
+            print("Unknown topic")
+    
+    def led_callback(self, msg):
+        if msg == b"on":
+            print("Got on")
+            self.led.value(1)
+        elif msg == b"off":
+            print("Got off")
+            self.led.value(0)
+
+
+def main():
+    micropython.alloc_emergency_exception_buf(100)
+    
+    sensor = dht.DHT22(machine.Pin(4))
+    led = machine.Pin(25, machine.Pin.OUT, value=1)
+    mqtt_client = umqtt.robust.MQTTClient(
+        client_id=MQTT_CLIENT_ID, server=MQTT_SERVER, port=MQTT_PORT
+    )
+    dioder = Dioder(led)
+
+    mqtt_client.connect()
+    mqtt_client.set_callback(dioder.message_callback)
+    mqtt_client.subscribe(MQTT_TOPIC_CTRL)
+    mqtt_client.subscribe(MQTT_TOPIC_BAZINGA)
+
+    tick = Tick(
+        sensor=sensor,
+        mqtt_client=mqtt_client,
+        topic_temp=MQTT_TOPIC_TEMP,
+        topic_hum=MQTT_TOPIC_HUM,
+    )
+
+    tim = machine.Timer(-1)
+    tim.init(period=5000, mode=machine.Timer.PERIODIC, callback=tick.do)
+
+    while True:
+        try:
+            mqtt_client.wait_msg()
+        except Exception as e:
+            print("Failed process the message: %s" % e)
+
+        # try:
+        #     cycle.do()
+        # except Exception as e:
+        #     print("Failed to perform the cycle: %s" % e)
+        #     machine.reset()
+        # time.sleep(5)
+
+
+main()
